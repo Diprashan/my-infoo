9e74affd707b7e68c6f1bf781a4247f9db69befd
Diprashan Basukala <diprashan1226@gmail.com>
2025-10-30 10:15:53 +0545

    Update main.js

--- main.js
+++ main.js
@@ -1,37 +1,7 @@
-let menuIcon = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.navbar');
-let sections = document.querySelectorAll('section');
-let navLinks = document.querySelectorAll('header nav a');
-
-// Scroll section active
-window.onscroll = () => {
-    let top = window.scrollY;
-    sections.forEach(sec => {
-        let offset = sec.offsetTop - 100; // mobile-friendly
-        let height = sec.offsetHeight;
-        let id = sec.getAttribute('id');
-
-        if (top >= offset && top < offset + height) {
-            navLinks.forEach(link => link.classList.remove('active'));
-            document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
-        }
-    });
-
-    // Close navbar on scroll (optional)
-    navbar.classList.remove('active');
-    menuIcon.classList.remove('bx-x');
-}
-
-// Toggle navbar
-menuIcon.onclick = () => {
-    menuIcon.classList.toggle('bx-x');
-    navbar.classList.toggle('active');
-}
-
-// Close navbar on link click (mobile)
 navLinks.forEach(link => {
     link.onclick = () => {
         navbar.classList.remove('active');
         menuIcon.classList.remove('bx-x');
     }
 });
+let offset = sec.offsetTop - 100;
